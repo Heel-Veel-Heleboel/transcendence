37a02916085ebca4e7f3e54d8a7e7541a954b6eb
Vova <vovbi4@gmail.com>
2025-10-29 14:59:33 +0100

    Process code review comments

--- vitest.config.ts
+++ vitest.config.ts
@@ -20,10 +20,10 @@ export default defineConfig({
       ],
       // Coverage thresholds
       thresholds: {
-          branches: 75,
-          functions: 85,
-          lines: 85,
-          statements: 85
+          branches: 80,
+          functions: 80,
+          lines: 80,
+          statements: 80
       }
     },
     
