78e950dd927ca0bcc666270da693aec9994142b5
spenning <whimizera@mailbox.org>
2025-11-11 12:18:33 +0100

    basic typescript interaction

--- src/frontend/src/main.ts
+++ src/frontend/src/main.ts
@@ -1,23 +1,19 @@
-import typescriptLogo from './typescript.svg';
-import viteLogo from '/vite.svg';
-import { setupCounter } from './counter.ts';
+const initApp = (): void => {
+  document.getElementById('optionCool')?.addEventListener('click', function () {
+    window.location.href = 'https://nu.nl';
+  });
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
+  document
+    .getElementById('optionStart')
+    ?.addEventListener('click', function () {
+      window.location.href = 'https://www.ad.nl/';
+    });
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!);
+  document
+    .getElementById('optionLogin')
+    ?.addEventListener('click', function () {
+      window.location.href = 'https://ra.co/';
+    });
+};
+
+document.addEventListener('DOMContentLoaded', initApp);
