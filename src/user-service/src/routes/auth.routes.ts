e8775480bbcbd9b5b651c00013354e041183805b
Artem <amysiv9@gmail.com>
2025-11-12 10:50:42 +0100

    create simple registration controller

--- src/user-service/src/routes/auth.routes.ts
+++ src/user-service/src/routes/auth.routes.ts
@@ -1,10 +1,9 @@
 import { FastifyInstance } from 'fastify';
+import { registerUserController } from  '../controllers/auth.controller.js';
 
 export default async function authRoutes(fastify: FastifyInstance) {
 
-  fastify.post('/register', async () => {
-    return { message: 'User registered successfully' };
-  });
+  fastify.post('/register',  registerUserController);
 
   fastify.post('/login', async () => {
     return { message: 'User logged in successfully' };
