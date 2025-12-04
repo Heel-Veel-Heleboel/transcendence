e877e9b5f53634b043f0c2b6e3692f8e1f5f972e
spenning <whimizera@mailbox.org>
2025-12-04 17:51:01 +0100

    updated main.mock.jsdom.ts

--- test/frontend/main.mock.jsdom.ts
+++ test/frontend/main.mock.jsdom.ts
@@ -1,15 +1,13 @@
 import { describe, it, expect, vi } from 'vitest';
 import * as module from '../../src/frontend/src/main.ts';
 
-describe('initApp', () => {
-  it('calls each register function once', () => {
-    const spyCool = vi.spyOn(module, 'registerOptionCool');
-    const spyGame = vi.spyOn(module, 'registerOptionGame');
-    const spyLogin = vi.spyOn(module, 'registerOptionLogin');
-    module.initApp();
+//method: https://stackoverflow.com/questions/72277787/spying-on-mocking-import-of-an-import
+vi.mock('../../src/frontend/src/state.ts');
 
-    expect(spyCool).toHaveBeenCalledOnce();
-    expect(spyGame).toHaveBeenCalledOnce();
-    expect(spyLogin).toHaveBeenCalledOnce();
+describe('initApp', () => {
+  it('calls each register function once', async () => {
+    const { loadTemplate } = await import('../../src/frontend/src/state.ts');
+    module.initApp();
+    expect(loadTemplate).toHaveBeenCalledWith('login');
   });
 });
