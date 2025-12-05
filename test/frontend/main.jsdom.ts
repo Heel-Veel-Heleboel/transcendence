3a39dc5fc5f2d3d8cb495c476188131cc47febf1
spenning <whimizera@mailbox.org>
2025-12-05 14:07:49 +0100

    refactored main tests

--- test/frontend/main.jsdom.ts
+++ test/frontend/main.jsdom.ts
@@ -2,6 +2,10 @@ import { describe, it, expect, vi, afterEach } from 'vitest';
 import '@testing-library/dom';
 import * as module from '../../src/frontend/src/main.ts';
 
+//method: https://stackoverflow.com/questions/72277787/spying-on-mocking-import-of-an-import
+vi.mock('../../src/frontend/src/state.ts');
+vi.mock('../../src/frontend/src/login.ts');
+
 describe('document EventListener', () => {
   afterEach(() => {
     vi.restoreAllMocks();
@@ -19,5 +23,14 @@ describe('document EventListener', () => {
     );
 
     expect(spy).toHaveBeenCalled();
+  });
+
+  it('initApp', async () => {
+    const { loadTemplate } = await import('../../src/frontend/src/state.ts');
+    const { initLogin } = await import('../../src/frontend/src/login.ts');
+
+    module.initApp();
+    expect(loadTemplate).toHaveBeenCalledWith('login');
+    expect(initLogin).toHaveBeenCalled();
   });
 });
