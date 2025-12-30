15f294dcaeed8fc1fa819f7a18e01285c1fe058a
spenning <whimizera@mailbox.org>
2025-12-30 15:21:59 +0100

    tests for game.ts

--- test/frontend/game.jsdom.ts
+++ test/frontend/game.jsdom.ts
@@ -1,166 +1,59 @@
-import { describe, it, expect, vi, beforeEach, afterEach } from 'vitest';
-import * as BABYLON from '@babylonjs/core';
-import * as module from '../../src/frontend/src/game/game.ts';
+import { describe, it, expect, vi, afterEach } from 'vitest';
+import { Game } from '../../src/frontend/src/game/systems/game.ts';
+import * as canvasModule from '../../src/frontend/src/game/utils/canvas.ts';
+import * as createModule from '../../src/frontend/src/game/utils/create.ts';
+import * as physicsModule from '../../src/frontend/src/game/utils/physics.ts';
+import * as eventModule from '../../src/frontend/src/game/utils/eventListeners.ts';
+import { NullEngine, Scene } from '@babylonjs/core';
 
-// source
-// https://github.com/BabylonJS/Babylon.js/tree/master/packages/dev/core/test/unit
-
-vi.mock('@babylonjs/core', async () => {
-  const actual =
-    await vi.importActual<typeof import('@babylonjs/core')>('@babylonjs/core');
-
-  return {
-    ...actual,
-    Engine: vi.fn((...args) => new actual.NullEngine(...args)) // redirect construction
-  };
-});
-
-function createMockCanvas() {
-  return '<canvas id="renderCanvas"></canvas>';
-}
-
-function createMockScene() {
-  const engine = new BABYLON.NullEngine();
-  const scene = new BABYLON.Scene(engine);
-  return scene;
-}
-
-describe('createScene', () => {
+describe('Game Class', () => {
   afterEach(() => {
     vi.restoreAllMocks();
   });
 
-  it('initGame', () => {
-    document.body.innerHTML = createMockCanvas();
-    const spyCanvas = vi.spyOn(module, 'getCanvas');
-    const spyEngine = vi.spyOn(module, 'createEngine');
-    const spyScene = vi.spyOn(module, 'createScene');
+  it('initGame', async () => {
+    const game = new Game();
 
-    module.initGame();
-
-    expect(spyCanvas).toBeCalled();
-    expect(spyEngine).toBeCalled();
-    expect(spyScene).toBeCalled();
-  });
-
-  it('createScene', () => {
-    const engine = new BABYLON.NullEngine();
-    const scene = module.createScene(engine);
-
-    expect(scene).toBeInstanceOf(BABYLON.Scene);
-  });
-
-  it('getCanvas', () => {
-    document.body.innerHTML = createMockCanvas();
-    const canvas = module.getCanvas();
-    expect(canvas).toBeInstanceOf(HTMLCanvasElement);
-  });
-
-  it('createEngine', () => {
-    document.body.innerHTML = createMockCanvas();
-    const canvas = module.getCanvas();
-    const engine = module.createEngine(canvas);
-    expect(engine).toBeInstanceOf(BABYLON.NullEngine);
-  });
-
-  it('createCamera', () => {
-    const scene = createMockScene();
-
-    const camera = module.createCamera(scene);
-    expect(camera).toBeInstanceOf(BABYLON.ArcRotateCamera);
-  });
-
-  it('createPlane', () => {
-    const name = 'test';
-    const height = 1;
-    const width = 1;
-    const scene = createMockScene();
-    const plane = module.createPlane(scene, name, height, width);
-
-    expect(plane).toBeInstanceOf(BABYLON.Mesh);
-    expect(plane.name).toBe(name);
-    expect(plane.sideOrientation).toBe(1);
-  });
-
-  it('createArena', () => {
-    const scene = createMockScene();
-    const arena = module.createArena(scene);
-
-    expect(Array.isArray(arena)).toBe(true);
-    expect(arena.every(item => item instanceof BABYLON.Mesh)).toBe(true);
-    expect(arena.length).toBe(6);
-  });
-
-  it('createBall', () => {
-    const scene = createMockScene();
-    const ball = module.createBall(scene, 0.1);
-
-    expect(ball).toBeInstanceOf(module.Ball);
-    expect(ball.mesh).toBeInstanceOf(BABYLON.Mesh);
-  });
-});
-
-describe('audio', () => {
-  let engine: BABYLON.NullEngine;
-  let scene: BABYLON.Scene;
-
-  beforeEach(() => {
-    engine = new BABYLON.NullEngine();
-    scene = new BABYLON.Scene(engine);
-  });
-
-  afterEach(() => {
-    scene.dispose();
-    (scene as any) = null;
-    engine.dispose();
-    (engine as any) = null;
-  });
-
-  it('createBgMusic', () => {
-    const bg = module.createBgMusic(scene);
-
-    expect(bg.loop).toBe(true);
-    expect(bg.autoplay).toBe(true);
-    expect(bg.name).toBe('mySong');
-  });
-});
-
-describe('utils', () => {
-  afterEach(() => {
-    vi.restoreAllMocks();
-  });
-
-  it('inverseCoordinate - negative input', () => {
-    expect(module.inverseCoordinate(-1)).toBe(1);
-  });
-
-  it('inverseCoordinate - 0 input', () => {
-    expect(module.inverseCoordinate(0)).toBe(-0);
-  });
-
-  it('inverseCoordinate - positive input', () => {
-    expect(module.inverseCoordinate(1)).toBe(-1);
-  });
-
-  it('inverseCoordinate - float negative input', () => {
-    expect(module.inverseCoordinate(-1.5)).toBe(1.5);
-  });
-
-  it('inverseCoordinate - float positive input', () => {
-    expect(module.inverseCoordinate(1.5)).toBe(-1.5);
-  });
-
-  it('inverseCoordinate - random 10 integers input', () => {
-    for (let i = 0; i < 10; i++) {
-      const arg = Math.floor(Math.random() * 100);
-      expect(module.inverseCoordinate(arg)).toBe(-arg);
-    }
-  });
-
-  it('inverseCoordinate - random 10 float input', () => {
-    for (let i = 0; i < 10; i++) {
-      const arg = Math.random();
-      expect(module.inverseCoordinate(arg)).toBe(-arg);
-    }
+    const canvasSpy = vi
+      .spyOn(canvasModule, 'getCanvas')
+      .mockImplementation(() => {
+        return document.createElement('canvas');
+      });
+    const engineSpy = vi
+      .spyOn(createModule, 'createEngine')
+      .mockImplementation(() => {
+        return new NullEngine();
+      });
+    const physicsSpy = vi
+      .spyOn(physicsModule, 'initializePhysics')
+      .mockImplementation(() => {
+        return Promise.resolve();
+      });
+    const importSpy = vi
+      .spyOn(canvasModule, 'prepareImportGLTF')
+      .mockImplementation(() => {});
+    const sceneSpy = vi.spyOn(game, 'initScene').mockImplementation(() => {
+      const engine = new NullEngine();
+      const scene = new Scene(engine);
+      return Promise.resolve(scene);
+    });
+    const resizeSpy = vi
+      .spyOn(eventModule, 'engineResizeListener')
+      .mockImplementation(() => {});
+    const debugSpy = vi
+      .spyOn(eventModule, 'debugLayerListener')
+      .mockImplementation(() => {});
+    const resolutionSpy = vi
+      .spyOn(canvasModule, 'initializeResolution')
+      .mockImplementation(() => {});
+    await game.initGame();
+    expect(canvasSpy).toHaveBeenCalled();
+    expect(engineSpy).toHaveBeenCalled();
+    expect(physicsSpy).toHaveBeenCalled();
+    expect(sceneSpy).toHaveBeenCalled();
+    expect(importSpy).toHaveBeenCalled();
+    expect(resizeSpy).toHaveBeenCalled();
+    expect(debugSpy).toHaveBeenCalled();
+    expect(resolutionSpy).toHaveBeenCalled();
   });
 });
