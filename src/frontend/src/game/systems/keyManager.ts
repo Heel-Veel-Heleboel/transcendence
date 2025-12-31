0bdd2f3083621e17b35d63fbe52f89ba54d9a3e8
sebastianpenning <whimizera@mailbox.org>
2025-12-31 17:26:33 +0100

    seperated keyManager utils

--- src/frontend/src/game/systems/keyManager.ts
+++ src/frontend/src/game/systems/keyManager.ts
@@ -1,6 +1,12 @@
 import { Scene, KeyboardInfo, KeyboardEventTypes } from '@babylonjs/core';
 import { IKeyManager } from '../types/types';
 import { Player } from '../components/player.ts';
+import {
+  checkLowerX,
+  checkUpperX,
+  checkLowerY,
+  checkUpperY
+} from '../utils/KeyManagerUtils.ts';
 
 export class KeyManager implements IKeyManager {
   public windowFrames: number;
@@ -37,53 +43,12 @@ export class KeyManager implements IKeyManager {
   }
 
   register(sequence: string[], coords: { x: number; y: number }): void {
-    // Normalize sequence: ["q","e"] → "q+e"
+    if (!sequence.length) return;
     this.actions.set(sequence.join('+'), coords);
   }
 
   handleKey(key: string): void {
-    console.log(key);
     this.buffer.push(key.toLowerCase());
-  }
-
-  checkUpperY(yMove: number) {
-    return (
-      this.player.physicsMesh.aggregate.transformNode.absolutePosition.y +
-        yMove >=
-      this.player.goalPosition.y +
-        this.player.goalDimensions.y / 2 -
-        this.player.goalDimensions.y / this.player.ratioDiv / 2
-    );
-  }
-
-  checkUpperX(xMove: number) {
-    return (
-      this.player.physicsMesh.aggregate.transformNode.absolutePosition.x +
-        xMove >=
-      this.player.goalPosition.x +
-        this.player.goalDimensions.x / 2 -
-        this.player.goalDimensions.x / this.player.ratioDiv / 2
-    );
-  }
-
-  checkLowerY(yMove: number) {
-    return (
-      this.player.physicsMesh.aggregate.transformNode.absolutePosition.y +
-        yMove <=
-      this.player.goalPosition.y -
-        this.player.goalDimensions.y / 2 +
-        this.player.goalDimensions.y / this.player.ratioDiv / 2
-    );
-  }
-
-  checkLowerX(xMove: number) {
-    return (
-      this.player.physicsMesh.aggregate.transformNode.absolutePosition.x +
-        xMove <=
-      this.player.goalPosition.x -
-        this.player.goalDimensions.x / 2 +
-        this.player.goalDimensions.x / this.player.ratioDiv / 2
-    );
   }
 
   handlePrecisionKey(key: string) {
@@ -92,28 +57,25 @@ export class KeyManager implements IKeyManager {
     const yMove = this.player.ratioDiv / 25;
     switch (index) {
       case 0:
-        if (this.checkUpperY(yMove)) {
+        if (checkUpperY(yMove, this.player)) {
           break;
         }
         this.player.movePrecise({ x: 0, y: yMove });
-        console.log(
-          this.player.physicsMesh.aggregate.transformNode.absolutePosition.y
-        );
         break;
       case 1:
-        if (this.checkLowerX(xMove)) {
+        if (checkLowerX(xMove, this.player)) {
           break;
         }
         this.player.movePrecise({ x: -xMove, y: 0 });
         break;
       case 2:
-        if (this.checkLowerY(yMove)) {
+        if (checkLowerY(yMove, this.player)) {
           break;
         }
         this.player.movePrecise({ x: 0, y: -yMove });
         break;
       case 3:
-        if (this.checkUpperX(xMove)) {
+        if (checkUpperX(xMove, this.player)) {
           break;
         }
         this.player.movePrecise({ x: xMove, y: 0 });
@@ -134,8 +96,6 @@ export class KeyManager implements IKeyManager {
       const coords = this.actions.get(sequenceKey);
       if (coords) this.player.move(coords);
     }
-    console.log('resolve: ' + sequenceKey);
-
     this.reset();
   }
 
