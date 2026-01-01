d65bbe8d7f48aaec53a7e137b70ae20e9bb2f3d8
sebastianpenning <whimizera@mailbox.org>
2026-01-01 17:29:18 +0100

    deleted unused test

--- test/frontend/eventListener.jsdom.ts
+++ test/frontend/eventListener.jsdom.ts
@@ -16,33 +16,4 @@ describe('create', () => {
 
     expect(spy).toHaveBeenCalled();
   });
-
-  // it('loops through all options and test all registration', async () => {
-  //   const registrations = {
-  //     Game: module.registerOptionGame,
-  //     Login: module.registerOptionLogin
-  //   };
-  //
-  //   for (const [key, value] of Object.entries(registrations)) {
-  //     const buttonId = ('option' + key) as string;
-  //     const callBackFunction = ('goto' + key) as never;
-  //     const setupFunction = value;
-  //     document.body.innerHTML = createMockButton(buttonId);
-  //     const user = userEvent.setup();
-  //     const spy = vi
-  //       .spyOn(module, callBackFunction)
-  //       .mockImplementation(() => {});
-  //     setupFunction();
-  //     const element = document.getElementById(buttonId);
-  //     if (!element) {
-  //       throw new Error('Button Element necessary for test is null');
-  //     }
-  //
-  //     await user.click(element);
-  //     expect(
-  //       spy,
-  //       `${callBackFunction} should be called with ${buttonId} button`
-  //     ).toHaveBeenCalled();
-  //   }
-  // });
 });
