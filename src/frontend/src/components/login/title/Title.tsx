5c09e40c32211bb19c5194e4e457c3bb0a9f7313
spenning <whimizera@mailbox.org>
2026-01-06 13:53:55 +0100

    basic react components

---
+++ src/frontend/src/components/login/title/Title.tsx
@@ -0,0 +1,4 @@
+
+import { JSX } from "react"
+
+export const Title = (): JSX.Element => { return (<h1>Title</h1>) }
--- src/frontend/src/pages/login/Login.tsx
+++ src/frontend/src/pages/login/Login.tsx
@@ -1,7 +1,24 @@
 import { JSX } from "react"
+import { Animation } from "../../components/login/animation/Animation"
+import { Title } from "../../components/login/title/Title"
+import { Logo } from "../../components/login/logo/Logo"
+import { MenuOption } from "../../components/login/menuOption/MenuOption"
 
 export const Login = (): JSX.Element => {
     return (
-        <h1>Login</h1>
+        <div className="h-full">
+            <h1>Login</h1>
+            <Animation />
+            <div className="startMenu">
+                <Title />
+                <Logo />
+                <div className="menuOptions">
+
+                    <MenuOption />
+                    <MenuOption />
+                </div>
+            </div>
+
+        </div>
     )
 }
