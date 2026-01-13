ef2ac34188b6d3f53962f4d330fdfe16e039afe0
sebastianpenning <whimizera@mailbox.org>
2026-01-13 20:01:55 +0100

    cleanup game

--- src/frontend/src/pages/Game.tsx
+++ src/frontend/src/pages/Game.tsx
@@ -1,38 +1,19 @@
 import { JSX } from 'react';
-import { Mesh, FreeCamera, Vector3, Scene, HemisphericLight, MeshBuilder } from '@babylonjs/core';
+import { Scene } from '@babylonjs/core';
 import SceneComponent from 'babylonjs-hook';
 import { GameClient } from '../game_client/systems/gameClient';
 
-let box: Mesh;
+export const Game = (): JSX.Element | null => {
+    return (
+        <div id="Game" className="h-full w-full">
+            <SceneComponent id='game-canvas' antialias onSceneReady={onSceneReady} onRender={onRender} adaptToDeviceRatio className="h-full w-full" />
+        </div>
+    );
+}
 
 const onSceneReady = async (scene: Scene) => {
     const game = new GameClient(scene);
     await game.initGame();
-    // // This creates and positions a free camera (non-mesh)
-    // var camera = new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
-    //
-    // // This targets the camera to scene origin
-    // camera.setTarget(Vector3.Zero());
-    //
-    // const canvas = scene.getEngine().getRenderingCanvas();
-    //
-    // // This attaches the camera to the canvas
-    // camera.attachControl(canvas, true);
-    //
-    // // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-    // var light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
-    //
-    // // Default intensity is 1. Let's dim the light a small amount
-    // light.intensity = 0.7;
-    //
-    // // Our built-in 'box' shape.
-    // box = MeshBuilder.CreateBox("box", { size: 2 }, scene);
-    //
-    // // Move the box upward 1/2 its height
-    // box.position.y = 1;
-    //
-    // // Our built-in 'ground' shape.
-    // MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
 }
 
 /**
@@ -46,12 +27,3 @@ const onRender = (scene: Scene) => {
     //     box.rotation.y += ((rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000));
     // }
 }
-
-export const Game = (): JSX.Element | null => {
-    return (
-        <div className="h-full w-full" id="Game">
-            <SceneComponent antialias onSceneReady={onSceneReady} onRender={onRender} adaptToDeviceRatio className="h-full w-full" id='my-canvas' />
-        </div>
-    );
-}
-
