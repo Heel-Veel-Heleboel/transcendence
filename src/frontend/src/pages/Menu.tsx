49c789ad3fa328b4694ead4f861697eb584930dc
sebastianpenning <whimizera@mailbox.org>
2026-01-15 14:54:38 +0100

    added localeDate

--- src/frontend/src/pages/Menu.tsx
+++ src/frontend/src/pages/Menu.tsx
@@ -8,23 +8,21 @@ export const Menu = (): JSX.Element => {
         <div id='Menu' className="w-full h-full flex flex-col">
             <Toolbar />
             <div id="backgroundImage" className="min-h-full flex flex-col bg-[url(/logan_4.jpg)]">
-
                 <Widgets />
                 <LiveChat />
-
             </div>
-
         </div>
     )
 }
 
 export function Toolbar(): JSX.Element {
+    const time = localeDate();
     return (
         <div id="toolbar" className="w-full flex justify-between bg-sky-500">
             {/* TODO update with own logo*/}
-            <div className="px-2">logo</div>
+            <div className="px-2 py-2">logo</div>
             {/* TODO change to realtime implementation*/}
-            <div className="">time</div>
+            <div className="py-2">{`${time.date} - ${time.time}`}</div>
             <div id="toolbarOptionsContainer" className="w-35 flex ">
                 <ToolbarOption id='profile' src='profile.png' />
                 <ToolbarOption id='settings' src='settings.png' />
@@ -43,6 +41,31 @@ export function ToolbarOption({ id, src }: { id: string, src: string }): JSX.Ele
     )
 }
 
+
+export function localeDate() {
+    const locale = 'en-GB';
+    const [today, setDate] = useState(new Date()); // Save the current date to be able to trigger an update
+
+    useEffect(() => {
+        const timer = setInterval(() => { // Creates an interval which will update the current data every minute
+            // This will trigger a rerender every component that uses the useDate hook.
+            setDate(new Date());
+        }, 1 * 1000);
+        return () => {
+            clearInterval(timer); // Return a funtion to clear the timer so that it will stop being called on unmount
+        }
+    }, []);
+
+    const date = today.toLocaleDateString(locale, { year: '2-digit', month: 'numeric', day: 'numeric' });
+    const time = today.toLocaleTimeString(locale, { hour: 'numeric', minute: 'numeric' });
+
+    return {
+        date,
+        time
+    };
+};
+
+
 export function Widgets(): JSX.Element {
     return (
         <div id="widgetContainer" className="p-2 min-h-1/2 min-w-full flex bg-sky-500/50 bg-clip-content">
@@ -55,7 +78,7 @@ export function Widgets(): JSX.Element {
             */}
 
             <Widget title="speedmatching" logoPath="matchmaker.png" child={Speedmatching()} />
-            <Widget title="gymkhana" logoPath="gymkhana.png" />
+            <Widget title="gymkhana" logoPath="gymkhana.png" child={Gymkhana()} />
             <Widget title="trinityfetch" logoPath="crocodile.png" child={TrinityFetch()} />
             <Widget title="mtvx" logoPath='vinyl.png' child={Mtvx()} />
 
@@ -65,17 +88,54 @@ export function Widgets(): JSX.Element {
 
 }
 
+export function Gymkhana(): JSX.Element {
+    const quickPlayContent = (): JSX.Element => {
+        return (
+            <div>content</div>
+        )
+    }
+    const defaultContent = (): JSX.Element => {
+        return (
+            <div>content</div>
+        )
+    }
+    const customizedContent = (): JSX.Element => {
+        return (
+            <div>content</div>
+        )
+    }
+    return (
+        <GamesAvailable quickPlayContent={quickPlayContent()} defaultContent={defaultContent()} customizedContent={customizedContent()} />
+    )
+}
+
 export function Speedmatching(): JSX.Element {
+    const quickPlayContent = (): JSX.Element => {
+        return (
+            <div>content</div>
+        )
+    }
+    const defaultContent = (): JSX.Element => {
+        return (
+            <div>content</div>
+        )
+    }
+    const customizedContent = (): JSX.Element => {
+        return (
+            <div>content</div>
+        )
+    }
+    return (
+        <GamesAvailable quickPlayContent={quickPlayContent()} defaultContent={defaultContent()} customizedContent={customizedContent()} />
+    )
+}
+
+export function GamesAvailable({ quickPlayContent, defaultContent, customizedContent }: { quickPlayContent: JSX.Element, defaultContent: JSX.Element, customizedContent: JSX.Element }): JSX.Element {
     return (
         <div className="min-h-full flex flex-col">
-            <div className="grow border flex flex-col">
-                <div className="border">Quick Play</div>
-                <div className="grow"></div>
-            </div>
-            <div className="grow border">
-                <div className="border">Customized</div>
-                <div className="grow"></div>
-            </div>
+            <Terminal title='Quick Play' child={quickPlayContent} />
+            <Terminal title='Default' child={defaultContent} />
+            <Terminal title='Customized' child={customizedContent} />
         </div>
     );
 }
@@ -245,6 +305,45 @@ export function LiveChat(): JSX.Element {
     )
 }
 
+export function LiveChatRooms(): JSX.Element {
+    const roomsContent = (): JSX.Element => {
+        return (
+            <div>Room content</div>
+        )
+    }
+    return (
+        <div className="w-1/6 border">
+            <Terminal title="Rooms" child={roomsContent()} />
+        </div>
+    )
+}
+
+export function Chat(): JSX.Element {
+    const chatContent = (): JSX.Element => {
+        return (
+            <div>Chat content</div>
+        )
+    }
+    return (
+        <div className="w-4/6 border">
+            <Terminal title="Chat" child={chatContent()} />
+        </div>
+    )
+}
+
+export function LiveChatUsers(): JSX.Element {
+    const userContent = (): JSX.Element => {
+        return (
+            <div>user content</div>
+        )
+    }
+    return (
+        <div className="w-1/6 border">
+            <Terminal title="Users" child={userContent()} />
+        </div>
+    )
+}
+
 export function TitleBar({ logoPath, title }: { logoPath: string, title: string }): JSX.Element {
     return (
         <div className="titleBarBorders border-1 bg-lime-300 flex justify-between px-1">
@@ -272,20 +371,11 @@ export function TitleBar({ logoPath, title }: { logoPath: string, title: string 
     )
 }
 
-export function LiveChatRooms(): JSX.Element {
+export function Terminal({ title, child }: { title: string, child: JSX.Element }): JSX.Element {
     return (
-        <div className="w-1/6 border">Rooms</div>
-    )
-}
-
-export function Chat(): JSX.Element {
-    return (
-        <div className="w-4/6 border">Chat</div>
-    )
-}
-
-export function LiveChatUsers(): JSX.Element {
-    return (
-        <div className="w-1/6 border">Users</div>
+        <div className="grow flex flex-col">
+            <div className="border">{title}</div>
+            <div className="grow">{child}</div>
+        </div>
     )
 }
