b4d5d2b6672d79f9b214c807a36839ebf68c3c42
Artem <amysiv9@gmail.com>
2026-01-16 17:44:48 +0100

    feat: Implement login functionality in AuthController

--- src/auth/src/controllers/auth.ts
+++ src/auth/src/controllers/auth.ts
@@ -1,6 +1,10 @@
 import { AuthService } from '../services/auth.js';
 import { FastifyRequest, FastifyReply } from 'fastify';
+<<<<<<< HEAD
 import { RegisterDto, SafeUserDto, LoginDto, LoggedInUserDto } from '../types/dtos/auth.js';
+=======
+import { RegisterDto, SafeUserDto, LoginDto } from '../types/dtos/auth.js';
+>>>>>>> 609ebfd (feat: Implement login functionality in AuthController)
 
 export class AuthController {
   constructor( 
@@ -16,7 +20,11 @@ export class AuthController {
   
   async login(request: FastifyRequest<{ Body: LoginDto }>, reply: FastifyReply) : Promise<FastifyReply> {
     const { email, password } = request.body;
+<<<<<<< HEAD
     const loggedInUser: LoggedInUserDto = await this.authService.login({ email, password });
+=======
+    const loggedInUser = await this.authService.login({ email: email, password: password });
+>>>>>>> 609ebfd (feat: Implement login functionality in AuthController)
     return reply.code(200).send(loggedInUser);
   }
 }
