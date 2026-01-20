c08b0192411ee434e97f097401e9288cd0e57b9e
spenning <whimizera@mailbox.org>
2026-01-20 15:21:14 +0100

    added credits

--- src/frontend/src/pages/StartMenu.tsx
+++ src/frontend/src/pages/StartMenu.tsx
@@ -14,7 +14,7 @@ export function GetPage({ page, redirect }: { page: number, redirect: (page: num
         case PAGE.LOGIN:
             return <CenterContainer children={Login()} />
         case PAGE.CREDITS:
-            return <CenterContainer children={Credits()} />
+            return <CenterContainer children={Credits({ redirect })} />
         default:
             return <DefaultStartMenu redirect={redirect} />
     }
@@ -28,7 +28,7 @@ export const StartMenu = (): JSX.Element => {
     }
 
     return (
-        <div id='StartMenu' className="min-h-full">
+        <div id='StartMenu' className="min-h-full grow">
             <Animation />
             <MainWindowContainer children={<GetPage page={page} redirect={redirect} />} />
         </div >
@@ -55,9 +55,75 @@ export function Login(): JSX.Element {
     )
 }
 
-export function Credits(): JSX.Element {
+export function Credits({ redirect }: { redirect: (page: number) => void }): JSX.Element {
     return (
-        <div className="text-white">credits</div>
+        <div className="flex flex-col justify-between min-h-full text-white">
+            <div />
+            <CreditsSectionMembers />
+            <CreditsSectionServices />
+            <MenuOption text="MENU" callback={() => redirect(PAGE.MENU)} />
+            <div />
+        </div>
+    )
+}
+
+export function CreditsSectionMembers(): JSX.Element {
+    return (
+        <div>
+            <CreditsSectionTitle title="Members" />
+            <CreditsSectionContent Key="" value="vshkonda" />
+            <CreditsSectionContent Key="" value="spenning" />
+            <CreditsSectionContent Key="" value="amysiv" />
+        </div>
+    )
+
+}
+
+export function CreditsSectionServices(): JSX.Element {
+    return (
+        <div>
+            <CreditsSectionTitle title="services" />
+            <CreditsSectionContent Key="api-gateway" value="vshkonda" />
+            <CreditsSectionContent Key="frontend" value="spenning" />
+            <CreditsSectionContent Key="user-management" value="amysiv" />
+            <CreditsSectionContent Key="auth" value="amysiv" />
+            <CreditsSectionContent Key="game-server" value="spenning" />
+            <CreditsSectionContent Key="observability" value="vshkonda" />
+            <CreditsSectionContent Key="matchmaking" value="vshkonda" />
+            <CreditsSectionContent Key="livechat" value="vshkonda" />
+        </div>
+    )
+
+}
+
+export function CreditsSectionContent({ Key, value }: { Key: string, value: string }) {
+    const split =
+        <div>
+            <div className="flex justify-between text-2xl">
+                <div className="font-orbi text-right w-3xs">{Key}</div>
+                <div className="w-5"></div>
+                <div className="font-orbi text-left w-3xs">{value}</div>
+            </div>
+        </div>;
+    const center =
+        <div>
+            <div className="flex justify-between text-2xl">
+                <div ></div>
+                <div className="font-orbi text-center w-3xs">{value}</div>
+                <div ></div>
+            </div>
+        </div>;
+
+    if (Key.length === 0)
+        return (center);
+
+    return (split);
+
+}
+
+export function CreditsSectionTitle({ title }: { title: string }) {
+    return (
+        <div className="font-orbi text-6xl text-center">{title}</div>
     )
 }
 
@@ -73,7 +139,7 @@ export function CenterContainer({ children }: { children: JSX.Element }): JSX.El
 
 export function MainWindowContainer({ children }: { children: JSX.Element }): JSX.Element {
     return (
-        <div className="min-h-full">
+        <div className="h-screen">
             {children}
         </div>
     )
