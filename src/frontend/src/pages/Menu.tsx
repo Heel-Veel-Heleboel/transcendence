7218bb40ea24afa61c98f25c2e020d28ca212d00
sebastianpenning <whimizera@mailbox.org>
2026-01-22 12:10:28 +0100

    added draft profile page

--- src/frontend/src/pages/Menu.tsx
+++ src/frontend/src/pages/Menu.tsx
@@ -59,7 +59,27 @@ export function MainWindowContainer({ children }: { children: JSX.Element }): JS
 
 export function Profile(): JSX.Element {
     return (
-        <div className="min-h-full min-w-full bg-emerald-500/50">Profile</div>
+        <div className="flex min-w-full min-h-full bg-emerald-500/50 text-center">
+            <div className="w-1/2 flex flex-col justify-around text-xl">
+                <div>
+                    <div>user-name</div>
+                    <div>alias</div>
+                    <div className=" flex justify-center">
+                        <img src="./snake_codec.png" alt="profile_pic" className="w-1/4 h-1/2" />
+                    </div>
+                </div>
+                <div></div>
+            </div>
+            <div className="w-1/2 flex flex-col justify-around text-xl">
+                <div>
+                    <div>user-name</div>
+                    <div>alias</div>
+                    <div>password</div>
+                    <div>email address</div>
+                </div>
+                <div></div>
+            </div>
+        </div>
     )
 
 }
