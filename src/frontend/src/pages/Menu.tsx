f1df0179fe46fd5349caadb1f3efc5084c99473c
sebastianpenning <whimizera@mailbox.org>
2026-01-22 12:13:17 +0100

    draft settings page

--- src/frontend/src/pages/Menu.tsx
+++ src/frontend/src/pages/Menu.tsx
@@ -86,7 +86,17 @@ export function Profile(): JSX.Element {
 
 export function Settings(): JSX.Element {
     return (
-        <div className="min-h-full min-w-full bg-purple-500/50">Settings</div>
+        <div className="flex min-h-full min-w-full bg-purple-500/50 text-center">
+            <div className="w-full flex flex-col justify-around text-xl">
+                <div>
+                    <div>language</div>
+                    <div>color-scheme</div>
+                    <div>background-animation</div>
+                    <div>etc.</div>
+                </div>
+                <div></div>
+            </div>
+        </div>
     )
 }
 
