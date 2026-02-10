cf800b1d8f38ad2060dff4639ba7e1379e121282
Artem <amysiv9@gmail.com>
2026-02-10 17:22:17 +0100

    feat(user-management): add user repository interfaces and DTOs; update .gitignore

---
+++ src/user-management/src/dto/user.ts
@@ -0,0 +1,40 @@
+import { ActivityStatus } from '../../generated/prisma/client.js';
+
+
+//create user dtos
+export interface CreateUserDto {
+  email: string;
+  name: string;
+}
+export interface CreatedUserDto {
+  id: number;
+}
+
+
+//delete user dtos
+export interface DeleteUserDto {
+  id: number;
+}
+
+
+//update user dtos
+export interface UpdateUserDto {
+  id: number;
+  email?: string;
+  name?: string;
+}
+export interface UpdatedUserStatusDto {
+  id: number;
+  activity_status: ActivityStatus;
+}
+
+
+//find user dtos
+export interface FindUniqueUserDto {
+  id?: number;
+  email?: string;
+  name?: string;
+}
+export interface FindManyUserDto {
+  activity_status?: ActivityStatus;
+}
