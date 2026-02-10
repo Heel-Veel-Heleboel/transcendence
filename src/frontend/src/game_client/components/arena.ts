db9146199db8c85bdb152d2d81c087ecea0b5228
sebastianpenning <whimizera@mailbox.org>
2026-02-10 15:08:45 +0100

    fix balls array

--- src/frontend/src/game_client/components/arena.ts
+++ src/frontend/src/game_client/components/arena.ts
@@ -44,40 +44,45 @@ export class Arena implements IArena {
 
   async initMesh(scene: Scene) {
     const model = ImportMeshAsync(gameConfig.arenaImportpath, scene);
-    await model.then(result => {
-      if (result.meshes.length !== gameConfig.arenaMeshesCount)
-        throw Error(Errors.INVALID_ARENA_FORMAT);
-      for (const index of result.meshes) {
-        const mesh = index as Mesh;
-        if (mesh.id === gameConfig.rootMesh) continue;
-        if (mesh.id === gameConfig.areneId) {
-          mesh.flipFaces(true);
+    await model
+      .then(result => {
+        if (result.meshes.length !== gameConfig.arenaMeshesCount)
+          throw Error(Errors.INVALID_ARENA_FORMAT);
+        for (const index of result.meshes) {
+          const mesh = index as Mesh;
+          if (mesh.id === gameConfig.rootMesh) continue;
+          if (mesh.id === gameConfig.areneId) {
+            mesh.flipFaces(true);
+          }
+          const material = new StandardMaterial('wireframe', scene);
+          material.wireframe = true;
+          mesh.material = material;
+          if (mesh.material) {
+            mesh.material.wireframe = true;
+          }
+          const aggregate = new PhysicsAggregate(
+            mesh,
+            PhysicsShapeType.MESH,
+            { mass: 0.0, restitution: 1.0, friction: 0.0 },
+            scene
+          );
+          aggregate.body.setAngularDamping(0.0);
+          aggregate.body.setLinearDamping(0.0);
+          if (mesh.id === gameConfig.areneId) {
+            this._arena = { mesh, aggregate };
+          } else if (mesh.id === gameConfig.goalId1) {
+            aggregate.body.setCollisionCallbackEnabled(true);
+            this.goal_1 = { mesh, aggregate };
+          } else if (mesh.id === gameConfig.goalId2) {
+            aggregate.body.setCollisionCallbackEnabled(true);
+            this.goal_2 = { mesh, aggregate };
+          }
         }
-        const material = new StandardMaterial('wireframe', scene);
-        material.wireframe = true;
-        mesh.material = material;
-        if (mesh.material) {
-          mesh.material.wireframe = true;
-        }
-        const aggregate = new PhysicsAggregate(
-          mesh,
-          PhysicsShapeType.MESH,
-          { mass: 0.0, restitution: 1.0, friction: 0.0 },
-          scene
-        );
-        aggregate.body.setAngularDamping(0.0);
-        aggregate.body.setLinearDamping(0.0);
-        if (mesh.id === gameConfig.areneId) {
-          this._arena = { mesh, aggregate };
-        } else if (mesh.id === gameConfig.goalId1) {
-          aggregate.body.setCollisionCallbackEnabled(true);
-          this.goal_1 = { mesh, aggregate };
-        } else if (mesh.id === gameConfig.goalId2) {
-          aggregate.body.setCollisionCallbackEnabled(true);
-          this.goal_2 = { mesh, aggregate };
-        }
-      }
-    });
+      })
+      .catch(error => {
+        console.error('failed to import arena mesh');
+        console.error(error);
+      });
   }
 }
 /* v8 ignore stop */
