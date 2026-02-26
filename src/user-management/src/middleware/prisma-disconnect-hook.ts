94148f9087d2b865fbb6c1da9ea07c0aaeb5fd94
Artem <amysiv9@gmail.com>
2026-02-26 10:53:33 +0100

    refactor prisma-client export

--- src/user-management/src/middleware/prisma-disconnect-hook.ts
+++ src/user-management/src/middleware/prisma-disconnect-hook.ts
@@ -1,6 +1,7 @@
-import { prisma } from '../db/prisma-client.js';
+import { getPrismaClient } from '../db/prisma-client.js';
 
 export default async function prismaDisconnectHook() {
+  const prisma = getPrismaClient();
   await prisma.$disconnect().then(() => {
     console.log('Prisma client disconnected successfully.');
   }).catch((error) => {
