f91f1e1c2f1093225eb2d192798551b5745a17ea
spenning <whimizera@mailbox.org>
2026-02-27 14:24:21 +0100

    added capitalization to error messages

--- src/frontend/src/game-client/utils/Error.ts
+++ src/frontend/src/game-client/utils/Error.ts
@@ -1,18 +1,19 @@
 const Errors = {
   // NOTE: KEYGRID
-  INVALID_KEYGRID_CONFIG: 'keygrid config has null value',
-  INVALID_MATRIX_LENGTH: 'columns and rows not of same length',
-  INVALID_KEYS_LENGTH: 'keys.length not equal to keys.columns or keys.rows',
-  DUPLICATE_KEY_VALUES: 'duplicate keys in rows and columns not allowed',
-  SURPASSING_KEY_LIMIT: 'surpassing the maximum amount of keys',
+  INVALID_KEYGRID_CONFIG: 'Keygrid config has null value',
+  INVALID_MATRIX_LENGTH: 'Columns and rows not of same length',
+  INVALID_KEYS_LENGTH: 'Keys.length not equal to keys.columns or keys.rows',
+  DUPLICATE_KEY_VALUES: 'Duplicate keys in rows and columns not allowed',
+  SURPASSING_KEY_LIMIT: 'Surpassing the maximum amount of keys',
 
   // NOTE: ARENA
-  INVALID_ARENA_FORMAT: 'arena wrongly formatted',
-  FAILED_ARENA_IMPORT: 'failed to import arena mesh',
+  INVALID_ARENA_FORMAT: 'Arena wrongly formatted',
+  MISSING_ARENA_MESH: 'Missing mesh(es) to complete arena initialization',
+  FAILED_ARENA_IMPORT: 'Failed to import arena mesh',
 
   // NOTE: HUD
-  FAILED_HUD_IMPORT: 'failed to import hud',
-  MISSING_HUD_CONTROL: 'missing necessary hud control'
+  FAILED_HUD_IMPORT: 'Failed to import hud',
+  MISSING_HUD_CONTROL: 'Missing necessary hud control'
 };
 
 export default Errors;
