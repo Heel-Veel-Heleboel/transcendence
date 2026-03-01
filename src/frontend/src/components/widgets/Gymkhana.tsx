2021b6a9272dcdb844972cbed9df1db401523222
spenning <whimizera@mailbox.org>
2026-03-01 10:48:01 +0100

    Basic LobbyRoom

--- src/frontend/src/components/widgets/Gymkhana.tsx
+++ src/frontend/src/components/widgets/Gymkhana.tsx
@@ -1,11 +1,36 @@
 import { JSX } from "react"
 import { GamesAvailable } from "../utils/MenuUtils"
 
+import { Client } from "@colyseus/sdk";
+import { useLobbyRoom } from "@colyseus/react";
+
+const client = new Client("ws://localhost:2567");
+
+function Lobby() {
+    const { rooms, error, isConnecting } = useLobbyRoom(
+        () => client.joinOrCreate("lobby"),
+    );
+
+    if (isConnecting) return <p>Connecting...</p>;
+    if (error) return <p>Error: {error.message}</p>;
+    console.log(rooms);
+
+    return (
+        <ul>
+            {rooms.map((room) => (
+                <li key={room.roomId}>
+                    {room.name} — {room.clients}/{room.maxClients} players
+                </li>
+            ))}
+        </ul>
+    );
+}
+
 /* v8 ignore start */
 export function Gymkhana(): JSX.Element {
     const quickPlayContent = (): JSX.Element => {
         return (
-            <div>content</div>
+            Lobby()
         )
     }
     const defaultContent = (): JSX.Element => {
