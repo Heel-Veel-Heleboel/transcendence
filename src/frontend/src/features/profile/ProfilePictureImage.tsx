86b1bf4b46abb4608843e68cb4369bc09faaaf0a
sebastianpenning <whimizera@mailbox.org>
2026-04-07 15:07:11 +0200

    added missing html id

--- src/frontend/src/features/profile/ProfilePictureImage.tsx
+++ src/frontend/src/features/profile/ProfilePictureImage.tsx
@@ -1,7 +1,7 @@
 
 export function ProfilePictureImage({ image }: { image: string }) {
     return (
-        <div className="flex justify-between">
+        <div id='profile-picture-container' className="flex justify-between">
             <div />
             <img src={image} alt="profile_pic" className="w-1/4 min-h-1/2" />
             <div />
