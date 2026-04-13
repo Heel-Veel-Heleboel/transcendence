0aa713532d471596a423fed54b1c611580596fd2
sebastianpenning <whimizera@mailbox.org>
2026-04-13 15:58:16 +0200

    fix importing messageform

--- src/frontend/src/features/live-chat/Chat.tsx
+++ src/frontend/src/features/live-chat/Chat.tsx
@@ -3,6 +3,7 @@ import { IChatMessage } from "../../shared/types/chat.ts";
 import { useChatService } from "../../components/providers/Chat.tsx";
 import { ChatContainer } from "./ChatContainer.tsx";
 import { RenderAckMessage, RenderMessage, RenderUnknownMessageType } from "./RenderMessage.tsx";
+import { MessageForm } from "./MessageForm.tsx";
 
 export function Chat({ channelId }: { channelId: string }): JSX.Element {
     const service = useChatService();
@@ -85,42 +86,4 @@ export function Chat({ channelId }: { channelId: string }): JSX.Element {
 }
 
 
-
-export function MessageForm({ channelId }: { channelId: string }) {
-    const service = useChatService();
-    const [content, setContent] = useState<string>('');
-
-    async function submit(event: FormEvent<HTMLFormElement>) {
-        event.preventDefault();
-
-        try {
-            await service.sendMessage({ channelId, content })
-            setContent('');
-        } catch (e: any) {
-            alert('failed to send message');
-            console.error(e);
-        }
-    };
-
-    return (
-        <div id="message-form" className="min-h-1/5">
-            <div className="min-h-full flex">
-                <form className="min-h-full min-w-full flex" onSubmit={submit}>
-                    <div className="min-w-4/5 min-h-full">
-                        <textarea
-                            id="message-input-element"
-                            name="message-input"
-                            className="border w-full min-h-full"
-                            value={content}
-                            onChange={e => setContent(e.target.value)}
-                        />
-                    </div>
-                    <div id="message-send-button" className="min-w-1/5 min-h-full">
-                        <button type="submit" className="border min-h-full w-full" >Send</button>
-                    </div>
-                </form>
-            </div>
-        </div >
-    )
-}
 
