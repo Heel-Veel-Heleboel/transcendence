6aad28d865cf5870f180de4805289d600bfa3607
Vova <vovbi4@gmail.com>
2026-04-13 18:49:26 +0200

    Update dockerfile and scripts

--- src/user-management/entrypoint-dev.sh
+++ src/user-management/entrypoint-dev.sh
@@ -3,6 +3,9 @@
 # INFO:give dev user access to data volume
 chown -R app:appgroup /data
 
+# INFO:sync schema to database
+npx prisma db push
+
 npx prisma studio --browser none --port 51212 &
 
 # INFO:run program from script argument as dev user
