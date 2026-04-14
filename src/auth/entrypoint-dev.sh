071049c2c7a9ae70c58b9863274f53fab2c57930
Vova Shkonda <95353106+vovashko@users.noreply.github.com>
2026-04-14 09:11:19 +0200

    Rearrange chown command in entrypoint-dev.sh
    Reorder the chown command to follow the schema sync.

--- src/auth/entrypoint-dev.sh
+++ src/auth/entrypoint-dev.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# INFO:give dev user access to data volume
-chown -R app:appgroup /data
-
 # INFO:sync schema to database
 npm run db:push
+
+# INFO:give dev user access to data volume
+chown -R app:appgroup /data
 
 npx prisma studio --browser none --port 51213 &
 
