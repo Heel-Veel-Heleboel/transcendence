34cbf4b7f74829ba1e058e893a31d2b53b6987d9
sebastianpenning <whimizera@mailbox.org>
2026-04-26 16:46:45 +0200

    home logo fix

--- src/frontend/src/components/layout/Toolbar.tsx
+++ src/frontend/src/components/layout/Toolbar.tsx
@@ -10,11 +10,13 @@ export function Toolbar(): JSX.Element {
     const navigate = useNavigate();
     const auth = useAuth();
     return (
-        <div id="toolbar" className="h-1/20 w-full flex justify-between bg-gradient-to-r from-violet-800 from-10% via-orange-500 via-80% to-zinc-400 to-90%">
-            {/* TODO update with own logo*/}
-            <div className="px-2 py-2" onClick={() => navigate(CONFIG.MENU_NAVIGATION)}>logo</div>
+        <div id="toolbar" className="h-1/25 w-full flex justify-between bg-gradient-to-r from-violet-800 from-10% via-orange-500 via-80% to-zinc-400 to-90%">
+            <div className="px-2 py-2 w-1/50 h-1/4" onClick={() => navigate(CONFIG.MENU_NAVIGATION)}>
+                <img src="/home.png" alt="home_logo" />
+            </div>
+
             <div className="py-2">{`${time.date} - ${time.time}`}</div>
-            <div id="toolbar-options" className="flex ">
+            <div id="toolbar-options" className="flex">
                 <ToolbarOption id='profile' src={CONFIG.PROFILE_LOGO} callback={() => navigate(CONFIG.USER_PROFILE_NAVIGATION)} />
                 <ToolbarOption id='logout' src={CONFIG.LOGOUT_LOGO} callback={() => auth.logOut()} />
             </div>
