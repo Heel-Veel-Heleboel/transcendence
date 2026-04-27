91e81ac8ae2b77acb14a2ad73f471af344e8dda8
sebastianpenning <whimizera@mailbox.org>
2026-04-27 10:42:58 +0200

    start menu animation revamp + credits fix

--- src/frontend/src/pages/StartMenu.tsx
+++ src/frontend/src/pages/StartMenu.tsx
@@ -2,8 +2,7 @@ import { JSX } from "react"
 import { useNavigate } from "react-router-dom"
 import { CONFIG } from "../shared/config/AppConfig.ts"
 import { Title } from "../features/entry/Title.tsx"
-import { Logo } from "../features/entry/Logo.tsx"
-import { MenuOption } from "../features/entry/MenuOption.tsx"
+import { MenuOptionStart } from "../features/entry/MenuOption.tsx"
 import { EntryContainer } from "../features/entry/LoginContainer.tsx"
 import { CREDITS_NAVIGATION, ENTRY_NAVIGATION, QUIT_REDIRECT } from "../shared/constants/navigation.ts"
 
@@ -16,11 +15,11 @@ export function StartMenu(): JSX.Element {
             <div id='start-menu-container' className="h-screen relative z-1 text-white text-center flex flex-col justify-between">
                 <div></div>
                 <Title />
-                <Logo />
+                { /* <Logo /> */}
                 <div >
-                    <MenuOption text={CONFIG.MENU_OPTION_LOGIN_TEXT} margin={CONFIG.MENU_OPTION_MARGIN} callback={() => navigate(ENTRY_NAVIGATION)} />
-                    <MenuOption text={CONFIG.MENU_OPTION_CREDITS_TEXT} margin={CONFIG.MENU_OPTION_MARGIN} callback={() => navigate(CREDITS_NAVIGATION)} />
-                    <MenuOption text={CONFIG.MENU_OPTION_QUIT_TEXT} margin={CONFIG.MENU_OPTION_MARGIN} callback={() => window.location.replace(QUIT_REDIRECT)} />
+                    <MenuOptionStart text={CONFIG.MENU_OPTION_LOGIN_TEXT} margin={CONFIG.MENU_OPTION_MARGIN} callback={() => navigate(ENTRY_NAVIGATION)} />
+                    <MenuOptionStart text={CONFIG.MENU_OPTION_CREDITS_TEXT} margin={CONFIG.MENU_OPTION_MARGIN} callback={() => navigate(CREDITS_NAVIGATION)} />
+                    <MenuOptionStart text={CONFIG.MENU_OPTION_QUIT_TEXT} margin={CONFIG.MENU_OPTION_MARGIN} callback={() => window.location.replace(QUIT_REDIRECT)} />
                 </div>
                 <div></div>
             </div>
