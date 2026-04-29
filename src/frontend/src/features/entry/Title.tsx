8d0e4c269657ba59d50f928bc35352a3b6a6cb6f
spenning <whimizera@mailbox.org>
2026-04-29 14:58:58 +0200

    frontend contrast tweaks

--- src/frontend/src/features/entry/Title.tsx
+++ src/frontend/src/features/entry/Title.tsx
@@ -1,5 +1,12 @@
 import { JSX } from "react";
 
 export function Title(): JSX.Element {
-    return (<h1 id='title' className="text-8xl font-monof text-metalgear">Counter-pong</h1>)
+    return (
+        <h1 id='title' className="text-8xl flex font-monof justify-center">
+            <div>
+                <p className="text-metalgear">Counter</p>
+                <p className="text-solidsnake">'</p>
+                <p className="text-liquidsnake">Pong</p>
+            </div>
+        </h1>)
 }
