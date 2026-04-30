245e5d07833b27ad7f813e82c2d18dfbd3bbc006
spenning <whimizera@mailbox.org>
2026-04-30 14:03:57 +0200

    lint fix

--- src/frontend/src/game-client/systems/GameClient.ts
+++ src/frontend/src/game-client/systems/GameClient.ts
@@ -170,18 +170,18 @@ export class GameClient {
 
     scene.onPointerObservable.add(pointerInfo => {
       switch (pointerInfo.type) {
-        case PointerEventTypes.POINTERDOUBLETAP:
-          if (this.gameMode === 'powerup' && this.prota.powerShots) {
-            const forwardRay = this.powerCamera.getForwardRay();
-            this.room.send('powershot', {
-              origin: forwardRay.origin,
-              direction: forwardRay.direction.scale(50)
-            });
-          }
+      case PointerEventTypes.POINTERDOUBLETAP:
+        if (this.gameMode === 'powerup' && this.prota.powerShots) {
+          const forwardRay = this.powerCamera.getForwardRay();
+          this.room.send('powershot', {
+            origin: forwardRay.origin,
+            direction: forwardRay.direction.scale(50)
+          });
+        }
 
-          break;
-        default:
-          break;
+        break;
+      default:
+        break;
       }
     });
     scene.clearColor = new Color4(0, 0, 0, 1);
